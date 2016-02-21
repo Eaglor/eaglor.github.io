d26f822618c0baadbce225bbdeb3aa8c680ff399
Daniel <danielsaka@hotmail.com>
2016-02-21 03:51:44 +0100

    Humongous update!!!

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+ 
+    $("nav a").click(function(event) {
+ 		
+ 		event.preventDefault();
+
+ 		$("nav ul > li").each(function() {
+ 			$(this).removeClass("selected");
+ 		});
+
+ 		$(this).parent("li").addClass("selected");
+
+        $("main").load($(this).attr("href") + " article");
+ 
+    });
+
+    $("nav li").click(function(event) {
+
+ 		$("nav ul > li").each(function() {
+ 			$(this).removeClass("selected");
+ 		});
+
+ 		$(this).addClass("selected");
+
+        $("main").load($(this).find("a").attr("href") + " article");
+ 
+    });
+
+    init();
+ 
+});
+
+function init() {
+	$("nav ul > li").first().trigger("click");
+}
