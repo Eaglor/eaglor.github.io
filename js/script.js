dd5416383148f6d9a04ea475a0ab0f4d73261c45
Daniel <danielsaka@hotmail.com>
2016-02-25 00:46:44 +0100

    last update fix

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ var _ratings_ = [];
 var _myRating_ = null;
 
 $(document).ready(function() {
- 
+
     $("nav a").click(function(event) {
- 		
+
  		event.preventDefault();
 
  		$("nav ul > li").each(function() {
@@ -12,9 +12,7 @@ $(document).ready(function() {
  		});
 
  		$(this).parent("li").addClass("selected");
-
-        $("main").load($(this).attr("href") + " article");
- 
+      $("#content").load($(this).attr("href") + " article");
     });
 
     $("nav li").click(function(event) {
@@ -24,54 +22,57 @@ $(document).ready(function() {
  		});
 
  		$(this).addClass("selected");
-
-        $("main").load($(this).find("a").attr("href") + " article");
- 
+      $("#content").load($(this).find("a").attr("href") + " article");
     });
-    
+
     init();
- 
+
 });
 
 function init() {
 	$("nav ul > li").first().trigger("click");
 
-_myRating_ = addRatingWidget();
-updateRating();
+  _myRating_ = addRatingWidget();
+  updateRating();
 }
 
-function addRatingWidget(){
-    var nrOfRatings = _ratings_.length;
-    
-    if(_ratings_ != null && _ratings_.length > 0){
-        var sum = _ratings_.reduce(function(pv, cv){
-            return pv + cv; }, 0);
-        _myRating_.setRating(Math.round((sum / nrOfRatings), false);
-        $("#var-star").text(Math.round(sum / nrOfRatings)* 10) / 10);
-        $("#var-person").text(nrOfRatings);
-    }else {
-        $("var-star").text(0);
-        $("var-person").text(0);
-    }
+function updateRating() {
+  var nrOfRatings = _ratings_.length;
+
+  if (_ratings_ != null && _ratings_.length > 0) {
+    var sum =  _ratings_.reduce(function(pv, cv) { return pv + cv; }, 0);
+    //var res = Math.round((sum / nrOfRatings) * 10) / 10;
+    //var res = Math.round(sum / nrOfRatings);
+    _myRating_.setRating(Math.round(sum / nrOfRatings) ,false);
+    $("#var-star").text(Math.round((sum / nrOfRatings) * 10) / 10);
+    $("#var-person").text(nrOfRatings);
+  } else {
+    $("#var-star").text(0);
+    $("#var-person").text(0);
+  }
 }
 
-function addRatingWidget(){
-    var ratingElement = document.querySelector('#rating-input');
-    var currentRating = 0;
-    var maxRating = 5;
-    var callback = function(rating) {
-        addRating(rating);
-    };
-    var r = rating(ratingElement, currentRating, maxRating, callback);
-    return r;
+function addRating(rating) {
+  _ratings_[_ratings_.length] = rating;
+  updateRating();
 }
 
-function updatePrtion(val){
-    document.querySelector(#"portion").innerHTML = val;
-                           var x = document.querySelectorAll("span[data-unit]");
-    var i;
-    for(i = 0; i < x.length; i++){
-        x[i].innerHTML = x[i].getAttribute("data-unit") * val;
-    }
-    
+function addRatingWidget() {
+  var ratingElement = document.querySelector('#rating-input');
+  var currentRating = 0;
+  var maxRating = 5;
+  var callback = function(rating) {
+    addRating(rating);
+  };
+  var r = rating(ratingElement, currentRating, maxRating, callback);
+  return r;
+}
+
+function updatePortion(val) {
+  document.querySelector("#portien").innerHTML = val;
+  var x = document.querySelectorAll("span[data-unit]");
+  var i;
+  for (i = 0; i < x.length; i++) {
+      x[i].innerHTML = x[i].getAttribute("data-unit") * val;
+  }
 }
